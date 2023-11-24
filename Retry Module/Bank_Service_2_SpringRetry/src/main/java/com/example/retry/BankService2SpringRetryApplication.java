17647e7b2e632289d99688f9b2c49875a3cd7e82
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-24 18:09:52 +0530

    initial commit

---
+++ Retry Module/Bank_Service_2_SpringRetry/src/main/java/com/example/retry/BankService2SpringRetryApplication.java
@@ -0,0 +1,13 @@
+package com.example.retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankService2SpringRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankService2SpringRetryApplication.class, args);
+	}
+
+}
